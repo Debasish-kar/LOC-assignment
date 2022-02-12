0c78392389017f500a4b5da9839c9434eb2a47e6
Debasish-kar <99554483+Debasish-kar@users.noreply.github.com>
2022-02-13 01:32:08 +0530

    Add files via upload

--- Q_04.c
+++ Q_04.c
@@ -3,15 +3,15 @@ int main()
 {
  float a,b;
 int op;
-printf("enter two numbers");
+printf("enter two numbers: ");
 scanf("%f%f",&a,&b);
 printf("1.Addition \n");   
 printf("2.Subtraction \n"); 
 printf("3.Multiplication \n"); 
 printf("4.Division \n");
- printf("enter your choice: ");
- scanf("%d",&op);
- 
+
+printf("Enter yor choice: ");
+scanf("%d",&op);
 switch(op)
 {
     case 1:
@@ -28,7 +28,7 @@ printf("division of %f and %f is: %f",a,b,a/b);
 break; 
 default:
 printf("choose correctly");
-  break;
+break;
 }
 return 0;
 }
